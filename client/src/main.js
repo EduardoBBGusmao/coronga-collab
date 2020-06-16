344f340d7884442b9d49d41c8f04f4c23fd661c1
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-16 06:54:35 -0300

    import codemirror globally

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
+import VueCodeMirror from 'vue-codemirror';
 import App from './App.vue';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
 
 const socket = new WebSocket('ws://192.168.0.16:3000');
 
 Vue.prototype.$socket = socket;
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCodeMirror);
 
 new Vue({
   render: (h) => h(App),
